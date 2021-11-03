ee141507ba9d3e6b2a6316ab6605f224161a39ab
drad <robertjdstoddard@gmail.com>
2021-11-03 12:11:10 -0600

    models date format and init files

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a user name!',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'You need to provide an email address!',
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+        virtuals: true,
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+})
+
+const User = model('User', UserSchema)
+
+module.exports = User
