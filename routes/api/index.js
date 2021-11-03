41672dd4c37be07a997f5271517b4048168e9ccb
drad <robertjdstoddard@gmail.com>
2021-11-03 13:00:57 -0600

    routes and controllers

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const thoughtRoutes = require('./thought-routes')
+const userRoutes = require('./user-routes')
+
+router.use('/thougts', thoughtRoutes)
+router.use('/reaction', thoughtRoutes)
+
+router.use('/users', userRoutes)
+router.use('/friends', userRoutes)
+
+module.exports = router
